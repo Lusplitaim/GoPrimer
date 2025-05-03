7db38dddfb000308174d632c6166212b18e3fdd1
Lusplitaim <Foolman.ru@yandex.ru>
2025-05-03 16:13:41 +0300

    max stock profit algo

--- algorithms/dynamic-programming/dp.go
+++ algorithms/dynamic-programming/dp.go
@@ -60,3 +60,61 @@ func min(a, b int) int {
 		return b
 	}
 }
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+func RemoveNthFromEnd(head *ListNode, n int) *ListNode {
+	if n <= 0 || head == nil {
+		return head
+	}
+
+	elForDeletion := head
+	prevEl := head
+	iteration := 1
+
+	for el := head; el != nil; el, iteration = el.Next, iteration+1 {
+		if iteration > n {
+			prevEl = elForDeletion
+			elForDeletion = elForDeletion.Next
+		}
+	}
+
+	if prevEl == elForDeletion {
+		head = prevEl.Next
+	} else if prevEl.Next == nil {
+		prevEl = nil
+	} else {
+		prevEl.Next = elForDeletion.Next
+	}
+
+	return head
+}
+
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/
+func MaxStockProfit(prices []int) int {
+	if len(prices) == 1 {
+		return 0
+	}
+
+	result := 0
+
+	localMin := prices[0]
+	localMax := prices[0]
+	for i := 1; i < len(prices); i++ {
+		if prices[i] >= localMax {
+			localMax = prices[i]
+		} else {
+			result += localMax - localMin
+			localMin = prices[i]
+			localMax = prices[i]
+		}
+	}
+
+	result += localMax - localMin
+
+	return result
+}
